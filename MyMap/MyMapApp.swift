adb3b251d59815d945d7c76f61de4e0340aa9a3a
hayashiyoshino <twinklestaryoshino@gmail.com>
2023-02-11 17:31:12 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by Yoshino Hayashi on 2023/02/11.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
