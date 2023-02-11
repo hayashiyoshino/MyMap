29c2aa618273e6e7642bc935cd0ddabf49e9ac17
hayashiyoshino <twinklestaryoshino@gmail.com>
2023-02-11 17:41:18 +0900

    feature: MapView.swift ファイル作成

---
+++ MyMap/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  MyMap
+//
+//  Created by Yoshino Hayashi on 2023/02/11.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
